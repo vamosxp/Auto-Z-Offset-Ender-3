20990e6e288b751b1ec0ebb6b8c4a0976cd75a9d
vamosxp <virajjpawar@gmail.com>
2025-02-15 17:52:09 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,100 @@
-# Auto-Z-Offset-Ender-3
+
+## What's different? <br>
+This is suited to cartesian bedslingers. This was originally designed for Euclid and Klippy type sensors and CoreXY's, and then adapted to be used with a bltouch. But I'm on an Ender 3 and I can't move the toolhead in Y. So I've designed an adapter to relocate the Z endstop and turn it into a pin endstop. And troubleshooting some of the differences.
+
+  ## New Requirements:
+  1) Physical Z-Endstop mounted as a pin - it is our reference point and is always Z 0.0 for calculations.
+  2) BLTouch as probe - the sensor to check the distance between endstop and bed to calc the offset
+  3) Accurate X and Y probe offsets
+  
+  ## Notes:
+   Your BLTouch and Nozzle should have litte to no offset in the Y position. We won't be able to move Y to find the Z-Endpin on a Bedslinger
+    unless you mount the switch to the bed. But that requires cable management and addressing bed heat. Possible, Better, But not free!
+   The bigger the offset, the bigger the pin has to be. Your Nozzle and BLTouch have to both be able to touch the pin off of the bed so
+   
+   ## Installation:
+
+      Login to your pi by ssh. Clone the repo to your homefolder with this command:
+
+        
+        git clone https://github.com/skyshadex/auto_z_offset.git
+        cd ~/auto_z_offset
+        ./install.sh
+        
+
+For further updates you can add it to moonraker's updated manager:
+
+<pre><code>
+[update_manager auto_offset_z]
+type: git_repo
+path: ~/auto_z_offset
+origin: https://github.com/skyshadex/auto_z_offset.git
+install_script: install.sh
+</code></pre>
+
+    
+
+## Original:<br>
+## This is a Klipper plugin for an auto calibration Z offset with a BLTouch (or possible inductive probe - check hints first!)
+
+## Why:<br>
+
+I was inspired to make this possible since i build my Voron V2.4 and wanted the ability to change my print surfaces without
+recalibrate my z offset for every surface and possible temp cause i'm also printing more than abs and didn't want to switch
+to a klicky probe for example :-) (yes - the way how the klicky probe handles the z offset is the same idea behind)
+
+## Requirements:<br>
+
+1) Physical Z-Endstop - it is our reference point and is always Z 0.0 for calculations.
+2) BLTouch as probe - the sensor to check the distance between endstop and bed to calc the offset
+3) QUAD_GANTRY_LEVEL or Z_TILT used (the plugin checks if qgl or z-tilt is applied to ensure gantry and bed are parallel for correct values)
+4) Ensure you set you x and y offset correct cause the plugin usses this offsets to move the bltouch over the endstop pin!
+
+## How it works:<br>
+
+The bltouch will probe two points - top of endstop and surface of your bed to messure the distance. Now we have one problem:
+the nozzle triggers the endstop the bltouch can only touch the top surface - to ensure a correct offset the plugin includes a move distance
+of 0.5mm (spec value from omron switches used in voron builts) which is the way the microsowitch in the endstop needs to trigger BUT:
+ususally a bit more than 0.5mm is needed to trigger correctly (~0.1x till ~0.2x in most cases depending on the used microswitch).
+This last bit can be set manually during a print probe by adjusting with babysteps until offset is correct and adding this to the 
+config section of the plugin.
+<pre><code>
+
+                                                    | |
+                                      Nozzle        | |  BLTouch
+                                      |_   _|        -
+                                        \_/               
+
+Bed                                      _ 
+--------------------------------------- | | Endstop Pin
+</code></pre>
+
+## Configuration for klipper:
+
+<pre><code>
+[auto_offset_z]
+center_xy_position:175,175      # Center of bed for example
+endstop_xy_position:233.5,358   # Physical endstop nozzle over pin
+speed: 100                      # X/Y travel speed between the two points
+z_hop: 10                       # Lift nozzle to this value after probing and for move
+z_hop_speed: 20                 # Hop speed of probe
+ignore_alignment: False         # Optional - this allows ignoring the presence of z-tilt or quad gantry leveling config section
+offset_min: -1                  # Optional - by default -1 is used - used as failsave to raise an error if offset is lower than this value
+offset_max: 1                   # Optional - by default 1 is used - used as failsave to raise an error if offset is higher than this value
+endstop_min: 0                  # Optional - by default disabled (0) - used as failsave to raise an error if endstop is lower than this value
+endstop_max: 0                  # Optional - by default disabled (0) - used as failsave to raise an error if endstop is higher than this value
+offsetadjust: 0.0               # Manual offset correction option - start with zero and optimize during print with babysteps
+                                  1) If you need to lower the nozzle from -0.71 to -0.92 for example your value is -0.21.
+                                  2) If you need to move more away from bed add a positive value.
+</code></pre>
+
+## Hints for use with an inductive probe:
+
+in general an inductive probe should also work this way if it is able to detect the endstop pin but it is not tested and you have to
+ensure the probe is really detecting the endstop pin and not the bed surface which is possible really close to the pin.
+
+## Last words:
+
+I'm not a software developer, only a guy which is playing arround to solve his problems or needs :-)
+The code - i'm really sure - can be improved but it is running without problems for me now since some weeks.
+If you wan't to give it a try have fun but note: if something is going wrong i'm not responsible for it!
---
+++ auto_offset_z.py
@@ -0,0 +1,182 @@
+# Perform an easy auto offset calibration with BLTouch or Probe and a pysical z endstop (cnc-style)
+#
+# Initially developed for use with BLTouch
+#
+# Copyright (C) 2022 Marc Hillesheim <marc.hillesheim@outlook.de>
+#
+# Version 0.0.4 / 12.03.2022
+#
+# This file may be distributed under the terms of the GNU GPLv3 license.
+
+from . import probe
+import math
+
+class AutoOffsetZCalibration:
+    def __init__(self, config):
+        self.printer = config.get_printer()
+        x_pos_center, y_pos_center = config.getfloatlist("center_xy_position", count=2)
+        x_pos_endstop, y_pos_endstop = config.getfloatlist("endstop_xy_position", count=2)
+        self.center_x_pos, self.center_y_pos = x_pos_center, y_pos_center
+        self.endstop_x_pos, self.endstop_y_pos = x_pos_endstop, y_pos_endstop
+        self.z_hop = config.getfloat("z_hop", default=10.0)
+        self.z_hop_speed = config.getfloat('z_hop_speed', 15., above=0.)
+        zconfig = config.getsection('stepper_z')
+        self.max_z = zconfig.getfloat('position_max', note_valid=False)
+        self.ignore_alignment = config.getboolean('ignore_alignment', False)
+        self.endstop_pin = zconfig.get('endstop_pin')
+        self.speed = config.getfloat('speed', 50.0, above=0.)
+        self.offsetadjust = config.getfloat('offsetadjust', 0.0)
+        self.offset_min = config.getfloat('offset_min', -1)
+        self.offset_max = config.getfloat('offset_max', 1)
+        self.endstop_min = config.getfloat('endstop_min', 0)
+        self.endstop_max = config.getfloat('endstop_max', 0)
+        self.gcode = self.printer.lookup_object('gcode')
+        self.gcode_move = self.printer.lookup_object('gcode_move')
+        self.gcode.register_command("AUTO_OFFSET_Z", self.cmd_AUTO_OFFSET_Z, desc=self.cmd_AUTO_OFFSET_Z_help)
+
+        # check if a bltouch is installed
+        if config.has_section("bltouch"):
+            bltouch = config.getsection('bltouch')
+            self.x_offset = bltouch.getfloat('x_offset', note_valid=False)
+            self.y_offset = bltouch.getfloat('y_offset', note_valid=False)
+            # check if a possible valid offset is set for bltouch
+            if ((self.x_offset == 0) and (self.y_offset == 0)):
+                raise config.error("AutoOffsetZ: Check the x and y offset [bltouch] - it seems both are zero and the BLTouch can't be at the same position as the nozzle :-)")
+            # check if bltouch is set as endstop
+            if ('virtual_endstop' in self.endstop_pin):
+                raise config.error("AutoOffsetZ: BLTouch can't be used as z endstop with this command - use a physical endstop instead.")
+
+        # check if a probe is installed
+        elif config.has_section("probe"):
+            probe = config.getsection('probe')
+            self.x_offset = probe.getfloat('x_offset', note_valid=False)
+            self.y_offset = probe.getfloat('y_offset', note_valid=False)
+            # check if a possible valid offset is set for probe
+            if ((self.x_offset == 0) and (self.y_offset == 0)):
+                raise config.error("AutoOffsetZ: Check the x and y offset from [probe] - it seems both are 0 and the Probe can't be at the same position as the nozzle :-)")
+            # check if probe is set as endstop
+            if ('virtual_endstop' in self.endstop_pin):
+                raise config.error("AutoOffsetZ: Probe can't be used as z endstop - use a physical endstop instead.")
+        else:
+            raise config.error("AutoOffsetZ: No bltouch or probe in configured in your system - check your setup.")
+
+        # check if qgl or ztilt is available
+        if config.has_section("quad_gantry_level"):
+            self.adjusttype = "qgl"
+        elif config.has_section("z_tilt"):
+            self.adjusttype = "ztilt"
+        elif self.ignore_alignment == 1:
+            self.adjusttype = "ignore"
+        else:
+            raise config.error("AutoOffsetZ: This can only be used if your config contains a section [quad_gantry_level] or [z_tilt].")
+
+    # custom round operation based mathematically instead of python default cutting off
+    def rounding(self,n, decimals=0):
+        expoN = n * 10 ** decimals
+        if abs(expoN) - abs(math.floor(expoN)) < 0.5:
+            return math.floor(expoN) / 10 ** decimals
+        return math.ceil(expoN) / 10 ** decimals
+
+    def cmd_AUTO_OFFSET_Z(self, gcmd):
+        # check if all axes are homed
+        toolhead = self.printer.lookup_object('toolhead')
+        curtime = self.printer.get_reactor().monotonic()
+        kin_status = toolhead.get_kinematics().get_status(curtime)
+        skip = 0
+
+        # debug output start #
+        #gcmd.respond_raw("AutoOffsetZ (Homeing Result): %s" % (kin_status))
+        # debug output end #
+
+        if ('x' not in kin_status['homed_axes'] or
+            'y' not in kin_status['homed_axes'] or
+            'z' not in kin_status['homed_axes']):
+            raise gcmd.error("You must home X, Y and Z axes first")
+
+        if self.adjusttype == "qgl":
+            # debug output start #
+            #gcmd.respond_raw("AutoOffsetZ (Alignment Type): %s" % (self.adjusttype))
+            # debug output end #
+
+            # check if qgl has applied
+            alignment_status = self.printer.lookup_object('quad_gantry_level').get_status(gcmd)
+            if alignment_status['applied'] != 1:
+                raise gcmd.error("AutoOffsetZ: You have to do a quad gantry level first")
+
+        elif self.adjusttype == "ztilt":
+            # debug output start #
+            #gcmd.respond_raw("AutoOffsetZ (Alignment Type): %s" % (self.adjusttype))
+            # debug output end #
+
+            # check if ztilt has applied
+            alignment_status = self.printer.lookup_object('z_tilt').get_status(gcmd)
+            if alignment_status['applied'] != 1:
+                raise gcmd.error("AutoOffsetZ: You have to do a z tilt first")
+
+        elif self.adjusttype == "ignore":
+            gcmd.respond_info("AutoOffsetZ: Ignoring alignment as you requested by config ...")
+        else:
+            raise config.error("AutoOffsetZ: Your printer has no config for [quad_gantry_level] or [z_tilt] which is needed to work correctly.")
+
+        # debug output start #
+        #gcmd.respond_raw("AutoOffsetZ (Alignment Result): %s" % (alignment_status))
+        # debug output end #
+
+        gcmd_offset = self.gcode.create_gcode_command("SET_GCODE_OFFSET",
+                                                      "SET_GCODE_OFFSET",
+                                                      {'Z': 0})
+        self.gcode_move.cmd_SET_GCODE_OFFSET(gcmd_offset)
+
+        # Move with probe or bltouch to endstop XY position and test surface z position
+        gcmd.respond_info("AutoOffsetZ: Probing endstop ...")
+        toolhead.manual_move([self.endstop_x_pos - self.x_offset, self.endstop_y_pos - self.y_offset], self.speed)
+        zendstop = self.printer.lookup_object('probe').run_probe(gcmd)
+        # Perform Z Hop
+        if self.z_hop:
+            toolhead.manual_move([None, None, self.z_hop], self.z_hop_speed)
+
+        # Move with probe or bltouch to center XY position and test surface z position
+        gcmd.respond_info("AutoOffsetZ: Probing bed ...")
+        toolhead.manual_move([self.center_x_pos - self.x_offset, self.center_y_pos - self.y_offset], self.speed)
+        zbed = self.printer.lookup_object('probe').run_probe(gcmd)
+        # Perform Z Hop
+        if self.z_hop:
+            toolhead.manual_move([None, None, self.z_hop], self.z_hop_speed)
+
+        # calcualtion offset
+        endstopswitch = 0.5
+        diffbedendstop = zendstop[2] - zbed[2]
+        #offset = (0 - diffbedendstop  + endstopswitch) + self.offsetadjust
+        offset = self.rounding((0 - diffbedendstop  + endstopswitch) + self.offsetadjust,3)
+
+        gcmd.respond_info("AutoOffsetZ:\nBed: %.3f\nEndstop: %.3f\nDiff: %.3f\nManual Adjust: %.3f\nTotal Calculated Offset: %.3f" % (zbed[2],zendstop[2],diffbedendstop,self.offsetadjust,offset,))
+
+        # failsave
+        if offset < self.offset_min or offset > self.offset_max:
+            raise gcmd.error("AutoOffsetZ: Your calculated offset is out of config limits! (Min: %.3f mm | Max: %.3f mm) - abort..." % (self.offset_min,self.offset_max))
+
+        if self.endstop_min != 0 and zendstop[2] < self.endstop_min:
+            raise gcmd.error("AutoOffsetZ: Your endstop value is out of config limits! (Min: %.3f mm | Meassured: %.3f mm) - abort..." % (self.endstop_min,zendstop[2]))
+
+        if self.endstop_max != 0 and zendstop[2] > self.endstop_max:
+            raise gcmd.error("AutoOffsetZ: Your endstop value is out of config limits! (Max: %.3f mm | Meassured: %.3f mm) - abort..." % (self.endstop_max,zendstop[2]))
+
+        self.set_offset(offset)
+
+    cmd_AUTO_OFFSET_Z_help = "Test endstop and bed surface to calcualte g-code offset for Z"
+
+    def set_offset(self, offset):
+        # reset pssible existing offset to zero
+        gcmd_offset = self.gcode.create_gcode_command("SET_GCODE_OFFSET",
+                                                      "SET_GCODE_OFFSET",
+                                                      {'Z': 0})
+        self.gcode_move.cmd_SET_GCODE_OFFSET(gcmd_offset)
+        # set new offset
+        gcmd_offset = self.gcode.create_gcode_command("SET_GCODE_OFFSET",
+                                                      "SET_GCODE_OFFSET",
+                                                      {'Z': offset})
+        self.gcode_move.cmd_SET_GCODE_OFFSET(gcmd_offset)
+
+
+def load_config(config):
+    return AutoOffsetZCalibration(config)
